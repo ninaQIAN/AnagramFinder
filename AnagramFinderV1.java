b206134cb63e0fa10a371972b0357d3836020267
ninaQIAN <endingqian@hotmail.com>
2018-01-02 19:20:04 -0500

    clean up

--- AnagramFinderV1.java
+++ AnagramFinderV1.java
@@ -12,6 +12,44 @@ public class AnagramFinderV1 {
 		String fileName = args[0];
 		List<String> dictList = new ArrayList<String>();
 		List resultList = new ArrayList<String>();
+		String dictWord = null;
+		dictList = fetchDictionaryAsList(fileName);
+		while(true) {
+			// message
+			System.out.println("please enter a word or exit:");
+			// read string from command line
+			String word = System.console().readLine();
+			if("exit".equalsIgnoreCase(word)) {
+				System.out.println("exit!");
+				System.exit(0);
+			}
+			
+			// calculation
+			resultList = new ArrayList<String>();
+			long t3 = System.currentTimeMillis();
+			for (int i=0; i<dictList.size(); i++) { // compare with every word from dictionary
+				dictWord = dictList.get(i);
+				if(isAnagram(word.toLowerCase(), dictWord.toLowerCase())) {
+					resultList.add(dictWord);
+				}
+			}
+			long t4 = System.currentTimeMillis();
+			// output result
+			if(resultList.size()>0) { // has anagram
+				System.out.println(resultList.size() + " Anagrams found for " + word + " in " + (t4-t3) + "ms:\n"
+						+ String.join(",", resultList) + "\n");
+			}
+			else {
+				System.out.println("No anagrams found for " + word +" in " + (t4-t3) + "ms\n");
+			}
+		}
+	}
+	
+	/**
+	 * load dictionary as a ArrayList
+	 */
+	private static List fetchDictionaryAsList(String fileName) {
+		List returnList = new ArrayList<String>();
 		BufferedReader br = null;
 		String dictWord = null;
 		try {
@@ -19,50 +57,21 @@ public class AnagramFinderV1 {
 			br = new BufferedReader(new FileReader(fileName));
 			long t1 = System.currentTimeMillis();
 			while ((dictWord = br.readLine()) != null) {
-				dictList.add(dictWord);
+				returnList.add(dictWord);
 			}
 			long t2 = System.currentTimeMillis();
 			System.out.println("Dictionary loaded in " + (t2-t1) + "ms");
-			while(true) {
-				// message
-				System.out.println("please enter a word or exit:");
-				// read string from command line
-				String word = System.console().readLine();
-				if("exit".equalsIgnoreCase(word)) {
-					System.out.println("exit!");
-					System.exit(0);
-				}
-
-				// calculation
-				resultList = new ArrayList<String>();
-				long t3 = System.currentTimeMillis();
-				for (int i=0; i<dictList.size(); i++) { // compare with every word from dictionary
-					dictWord = dictList.get(i);
-					if(isAnagram(word.toLowerCase(), dictWord.toLowerCase())) {
-						resultList.add(dictWord);
-					}
-				}
-				long t4 = System.currentTimeMillis();
-				// output result
-				if(resultList.size()>0) { // has anagram
-					System.out.println(resultList.size() + " Anagrams found for " + word + " in " + (t4-t3) + "ms:\n"
-						+ String.join(",", resultList) + "\n");
-				}
-				else {
-					System.out.println("No anagrams found for " + word +" in " + (t4-t3) + "ms\n");
-				}
-			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				if(br!=null)
 					br.close();
-			} catch (IOException ioe)
-			{
+			} catch (IOException ioe) {
 				System.out.println("Error in closing the BufferedReader");
 			}
 		}
+		return returnList;
 	}
 
 	/**
@@ -91,4 +100,5 @@ public class AnagramFinderV1 {
 		}
 		return true;
 	}
+
 }
