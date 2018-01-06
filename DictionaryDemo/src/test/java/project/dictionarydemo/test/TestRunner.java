9310745f46deecc6d5cd74600d2a74e7a5f3c180
ninaQIAN <endingqian@hotmail.com>
2018-01-05 21:42:27 -0500

    modify test result output. System.out.println doesn't work because it's monitored by tests. use log.

--- DictionaryDemo/src/test/java/project/dictionarydemo/test/TestRunner.java
+++ DictionaryDemo/src/test/java/project/dictionarydemo/test/TestRunner.java
@@ -1,20 +1,33 @@
 package project.dictionarydemo.test;
 
+import java.util.logging.Logger;
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 import project.dictionarydemo.testcase.AnagramFinderTest;
+import project.dictionarydemo.testcase.DictionaryTest;
+import project.dictionarydemo.testcase.SearchTest;
 
 
 public class TestRunner {
-	   public static void main(String[] args) {
-	      Result result = JUnitCore.runClasses(AnagramFinderTest.class);
-			
-	      for (Failure failure : result.getFailures()) {
-	         System.out.println(failure.toString());
-	      }
-			
-	      System.out.println(result.wasSuccessful());
-	   }
-	} 
+	private static Logger log = Logger.getLogger(TestRunner.class.getName());
+	
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(AnagramFinderTest.class, DictionaryTest.class, SearchTest.class);
+		
+		log.info("TEST RESULT:\n" + result.getRunCount() + " test cases. " + result.getRunTime() + " ms\n");
+		for (Failure failure : result.getFailures()) {
+			log.info("ERROR: " + failure.toString());
+		}
+		if(result.wasSuccessful()) {
+			log.info("Successful.");
+			System.exit(0);
+		}
+		else {
+			log.info("Not successful.");
+			System.exit(1);
+		}
+	}
+} 
